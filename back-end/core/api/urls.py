0c6437e279ac1d3844b3d298593bb1aab8ff8c1a
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-10-23 14:41:23 +0300

    Update urls.py

--- back-end/core/api/urls.py
+++ back-end/core/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
+from core.api.views import ListTeams, AllSeasonMatches, NextUpcomingMatches, GamedayMatches, SelectedTeam
+
+
 urlpatterns = [
-    # path('<int:pk>/',),
-    # path('',)
+    path('teams/', ListTeams.as_view()),
+    path('teams/<int:pk>', SelectedTeam.as_view()),
+    path('all_season_matches_by_rounds/', AllSeasonMatches.as_view()),
+    path('next_upcoming_matches/', NextUpcomingMatches.as_view()),
+    path('gameday_matches/', GamedayMatches.as_view()),
 ]
