1084804640422aaa861c080cd5855b50c0c75368
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-10-23 14:41:01 +0300

    Update views.py

--- back-end/core/api/views.py
+++ back-end/core/api/views.py
@@ -1,11 +1,36 @@
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from OpenLigaDbApi.api import API
+from core.football_data.bundesliga_data import \
+    get_bundesliga_teams_info, get_all_season_matches_info_by_rounds,\
+    get_next_upcoming_matches, get_gameday_matches, get_selected_team_info
 
 
-class ListGames(generics.ListAPIView):
-    pass
+class ListTeams(APIView):
+    def get(self, request):
+        data = get_bundesliga_teams_info()
+        return Response(data=data)
 
 
-class GameDetails(generics.RetrieveAPIView):
-    pass
+class AllSeasonMatches(APIView):
+    def get(self, request):
+        data = get_all_season_matches_info_by_rounds()
+        return Response(data=data)
+
+
+class NextUpcomingMatches(APIView):
+    def get(self, request):
+        data = get_next_upcoming_matches()
+        return Response(data=data)
+
+
+class GamedayMatches(APIView):
+    def get(self, request):
+        data = get_gameday_matches()
+        return Response(data=data)
+
+
+class SelectedTeam(APIView):
+    def get(self, request, pk):
+        data = get_selected_team_info(pk)
+        return Response(data=data)
